f2abb1cf5e17f5f19df34ebe36ec91faddbe4704
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-13 23:57:02 +0900

    Refactor: Update Board repositories

--- src/main/java/com/sparta/hirello/primary/board/repository/BoardMemberRepository.java
+++ src/main/java/com/sparta/hirello/primary/board/repository/BoardMemberRepository.java
@@ -1,9 +1,16 @@
 package com.sparta.hirello.primary.board.repository;
 
+import com.sparta.hirello.primary.board.entity.Board;
 import com.sparta.hirello.primary.board.entity.BoardMember;
+import com.sparta.hirello.primary.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardMemberRepository extends JpaRepository<BoardMember, Long>,
-        BoardMemberRepositoryCustom {
+import java.util.Optional;
+
+public interface BoardMemberRepository extends JpaRepository<BoardMember, Long> {
+
+     Optional<BoardMember> findByBoardAndUser(Board board, User user);
+
+     boolean existsByBoardAndUser(Board board, User invitedUser);
 
 }
--- src/main/java/com/sparta/hirello/primary/board/repository/BoardRepositoryCustom.java
+++ src/main/java/com/sparta/hirello/primary/board/repository/BoardRepositoryCustom.java
@@ -1,5 +1,12 @@
 package com.sparta.hirello.primary.board.repository;
 
+import com.sparta.hirello.primary.board.entity.Board;
+import com.sparta.hirello.primary.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface BoardRepositoryCustom {
-    
+
+    Page<Board> findByUser(User user, Pageable pageable);
+
 }
