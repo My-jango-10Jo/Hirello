4f02262a04789bf80186efad99901abb7addf539
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-16 01:16:17 +0900

    Refactor: Update BoardMemberResponse

--- src/main/java/com/sparta/hirello/primary/board/dto/response/BoardMemberResponse.java
+++ src/main/java/com/sparta/hirello/primary/board/dto/response/BoardMemberResponse.java
@@ -1,17 +1,20 @@
 package com.sparta.hirello.primary.board.dto.response;
 
 import com.sparta.hirello.primary.board.entity.BoardMember;
+import com.sparta.hirello.primary.board.entity.BoardRole;
 import lombok.Data;
 
 @Data
 public class BoardMemberResponse {
 
-    private Long userId;
     private Long boardId;
+    private Long userId;
+    private BoardRole boardRole; // [USER, MANAGER]
 
     private BoardMemberResponse(BoardMember boardMember) {
         this.userId = boardMember.getUser().getId();
         this.boardId = boardMember.getBoard().getId();
+        this.boardRole = boardMember.getBoardRole();
     }
 
     public static BoardMemberResponse of(BoardMember boardMember) {
