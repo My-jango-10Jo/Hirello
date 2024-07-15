bc43fa210e330d4f4f2be028ab8e02745060746f
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-15 16:24:34 +0900

    Feat: Implement CardRepositoryCustomImpl

--- src/main/java/com/sparta/hirello/primary/card/repository/CardRepositoryCustomImpl.java
+++ src/main/java/com/sparta/hirello/primary/card/repository/CardRepositoryCustomImpl.java
@@ -1,5 +1,6 @@
 package com.sparta.hirello.primary.card.repository;
 
+import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.hirello.primary.board.entity.Board;
 import com.sparta.hirello.primary.card.entity.Card;
@@ -7,8 +8,15 @@ import com.sparta.hirello.primary.progress.entity.Progress;
 import com.sparta.hirello.primary.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.sparta.hirello.primary.card.entity.QCard.card;
+import static com.sparta.hirello.secondary.util.RepositoryUtil.getTotal;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,32 +26,127 @@ public class CardRepositoryCustomImpl implements CardRepositoryCustom {
 
     @Override
     public Page<Card> findByBoard(Board board, Pageable pageable) {
-        return null;
+        // 데이터 조회 쿼리
+        List<Card> cards = queryFactory
+                .selectFrom(card)
+                .where(card.progress.board.eq(board))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 카운트 쿼리
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(card)
+                .where(card.progress.board.eq(board))
+                .fetchOne();
+
+        return new PageImpl<>(cards, pageable, getTotal(totalCount));
     }
 
     @Override
-    public Page<Card> findByBoardAndWorker(Board board, User user, Pageable pageable) {
-        return null;
+    public Page<Card> findByBoardAndWorker(Board board, User worker, Pageable pageable) {
+        // 데이터 조회 쿼리
+        List<Card> cards = queryFactory
+                .selectFrom(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.worker.eq(worker)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 카운트 쿼리
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.worker.eq(worker)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(cards, pageable, getTotal(totalCount));
+
     }
 
     @Override
     public Page<Card> findByBoardAndProgress(Board board, Progress progress, Pageable pageable) {
-        return null;
+        // 데이터 조회 쿼리
+        List<Card> cards = queryFactory
+                .selectFrom(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.progress.eq(progress)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 카운트 쿼리
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.progress.eq(progress)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(cards, pageable, getTotal(totalCount));
     }
 
     @Override
-    public Page<Card> findByBoardAndWorkerAndProgress(Board board, User user, Progress progress, Pageable pageable) {
-        return null;
+    public Page<Card> findByBoardAndWorkerAndProgress(Board board, User worker, Progress progress, Pageable pageable) {
+        // 데이터 조회 쿼리
+        List<Card> cards = queryFactory
+                .selectFrom(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.worker.eq(worker),
+                        card.progress.eq(progress)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 전체 카운트 쿼리
+        Long totalCount = queryFactory
+                .select(Wildcard.count)
+                .from(card)
+                .where(
+                        card.progress.board.eq(board),
+                        card.worker.eq(worker),
+                        card.progress.eq(progress)
+                )
+                .fetchOne();
+
+        return new PageImpl<>(cards, pageable, getTotal(totalCount));
     }
 
     @Override
-    public void decreaseOrderBetween(Progress progress, int startOrder, int endOrder) {
-
-    }
-
-    @Override
+    @Transactional
     public void increaseOrderBetween(Progress progress, int startOrder, int endOrder) {
+        queryFactory.update(card)
+                .set(card.order, card.order.add(1))
+                .where(
+                        card.progress.eq(progress),
+                        card.order.between(startOrder, endOrder)
+                )
+                .execute();
+    }
 
+    @Override
+    @Transactional
+    public void decreaseOrderBetween(Progress progress, int startOrder, int endOrder) {
+        queryFactory.update(card)
+                .set(card.order, card.order.subtract(1))
+                .where(
+                        card.progress.eq(progress),
+                        card.order.between(startOrder, endOrder)
+                )
+                .execute();
     }
 
 }
