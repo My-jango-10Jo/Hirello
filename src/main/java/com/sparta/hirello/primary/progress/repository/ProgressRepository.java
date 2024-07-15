ae80c26233259710221c74d56a24c161d909813c
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-15 11:46:26 +0900

    Refactor: Update ProgressRepository

--- src/main/java/com/sparta/hirello/primary/progress/repository/ProgressRepository.java
+++ src/main/java/com/sparta/hirello/primary/progress/repository/ProgressRepository.java
@@ -3,8 +3,8 @@ package com.sparta.hirello.primary.progress.repository;
 import com.sparta.hirello.primary.progress.entity.Progress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface ProgressRepository extends JpaRepository<Progress, Long>, ProgressRepositoryCustom {
 
-public interface ProgressRepository extends JpaRepository<Progress, Long> {
-   Optional<Progress> findByProgressName(String progressName);
+    boolean existsByTitle(String title);
+
 }
---
+++ src/main/java/com/sparta/hirello/primary/progress/repository/ProgressRepositoryCustomImpl.java
@@ -0,0 +1,37 @@
+//package com.sparta.hirello.primary.progress.repository;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import com.sparta.hirello.primary.board.entity.Board;
+//import com.sparta.hirello.primary.progress.repository.ProgressRepositoryCustom;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static com.sparta.hirello.primary.progress.entity.QProgress.progress;
+//
+//@Repository
+//@RequiredArgsConstructor
+//public class ProgressRepositoryCustomImpl implements ProgressRepositoryCustom {
+//
+//    private final JPAQueryFactory queryFactory;
+//
+//    @Override
+//    @Transactional
+//    public void incrementOrderBetween(Board board, int startOrder, int endOrder) {
+//        queryFactory.update(progress)
+//                .set(progress.order, progress.order.add(1))
+//                .where(progress.board.eq(board)
+//                        .and(progress.order.between(startOrder, endOrder)))
+//                .execute();
+//    }
+//
+//    @Override
+//    @Transactional
+//    public void decrementOrderBetween(Board board, int startOrder, int endOrder) {
+//        queryFactory.update(progress)
+//                .set(progress.order, progress.order.subtract(1))
+//                .where(progress.board.eq(board)
+//                        .and(progress.order.between(startOrder, endOrder)))
+//                .execute();
+//    }
+//}
