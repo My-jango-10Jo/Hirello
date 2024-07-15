32e09dbf273055fd04d49765b6e5c72667f47625
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-15 15:51:04 +0900

    Refactor: Update CardRepositoryCustomImpl

--- src/main/java/com/sparta/hirello/primary/card/repository/CardRepositoryCustomImpl.java
+++ src/main/java/com/sparta/hirello/primary/card/repository/CardRepositoryCustomImpl.java
@@ -3,6 +3,8 @@ package com.sparta.hirello.primary.card.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.hirello.primary.board.entity.Board;
 import com.sparta.hirello.primary.card.entity.Card;
+import com.sparta.hirello.primary.progress.entity.Progress;
+import com.sparta.hirello.primary.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +19,31 @@ public class CardRepositoryCustomImpl implements CardRepositoryCustom {
     @Override
     public Page<Card> findByBoard(Board board, Pageable pageable) {
         return null;
+    }
+
+    @Override
+    public Page<Card> findByBoardAndWorker(Board board, User user, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Card> findByBoardAndProgress(Board board, Progress progress, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Page<Card> findByBoardAndWorkerAndProgress(Board board, User user, Progress progress, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public void decreaseOrderBetween(Progress progress, int startOrder, int endOrder) {
+
+    }
+
+    @Override
+    public void increaseOrderBetween(Progress progress, int startOrder, int endOrder) {
+
     }
 
 }
