7be09dc48be6347eddaca8edc3f9522bc2f8f7b0
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-16 01:17:04 +0900

    Feat: Add findByBoardOrderByOrder

--- src/main/java/com/sparta/hirello/primary/progress/repository/ProgressRepository.java
+++ src/main/java/com/sparta/hirello/primary/progress/repository/ProgressRepository.java
@@ -1,7 +1,10 @@
 package com.sparta.hirello.primary.progress.repository;
 
+import com.sparta.hirello.primary.board.entity.Board;
 import com.sparta.hirello.primary.progress.entity.Progress;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -15,5 +18,7 @@ public interface ProgressRepository extends JpaRepository<Progress, Long>, Progr
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select p from Progress p where p.id = :id")
     Optional<Progress> findByIdWithPessimisticLock(Long id);
+
+    Page<Progress> findByBoardOrderByOrder(Board board, Pageable pageable);
 
 }
