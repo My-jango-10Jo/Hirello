0118182354bb3176e7411a97d80a27a261daf600
Unho Yeo <122009132+uh1205@users.noreply.github.com>
2024-07-15 15:48:27 +0900

    Refactor: Update InvalidProgressOrderException to InvalidOrderException

--- src/main/java/com/sparta/hirello/secondary/exception/InvalidProgressOrderException.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.hirello.secondary.exception;
-
-public class InvalidProgressOrderException extends RuntimeException {
-
-    public InvalidProgressOrderException() {
-        super("Invalid progress order");
-    }
-
-}
